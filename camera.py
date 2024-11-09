6662bc3f1c16fa11e10f38cc2964d9f7599475f1
hjuump <164741307+hjuump@users.noreply.github.com>
2024-11-09 21:35:41 +0900

    안정적인 버전으로 컴백

--- camera.py
+++ camera.py
@@ -2,19 +2,20 @@ import cv2
 import time
 import os
 
-def take_photo():
-    # 사진을 저장할 디렉토리 생성
+def take_photo_series(photo_count):
+    photo_paths = []
     if not os.path.exists("static/photos"):
         os.makedirs("static/photos")
 
-    camera = cv2.VideoCapture(0)
-    time.sleep(1)  # 카메라가 준비될 시간을 줌
-    ret, frame = camera.read()
-    camera.release()
-    
-    if ret:
-        photo_path = f"static/photos/photo_{int(time.time())}.jpg"
-        cv2.imwrite(photo_path, frame)
-        return photo_path  # 촬영된 사진의 파일 경로 반환
-    return None
+    for i in range(photo_count):
+        camera = cv2.VideoCapture(0)
+        time.sleep(1)  # 카메라가 준비되도록 잠시 대기
+        ret, frame = camera.read()
+        if ret:
+            photo_path = f"static/photos/photo_{int(time.time())}_{i}.jpg"
+            cv2.imwrite(photo_path, frame)
+            photo_paths.append(photo_path)
+        camera.release()
+        time.sleep(1)  # 사진 간격 조절
 
+    return photo_paths
