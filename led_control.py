1f8bf716df6f5fe3927c60456b8ad9d9e72c50bf
hjuump <164741307+hjuump@users.noreply.github.com>
2024-12-03 10:21:49 +0900

    LED 추가 및 조도에 따른 컨트롤 추가

--- led_control.py
+++ led_control.py
@@ -1,13 +1,29 @@
 import RPi.GPIO as GPIO
 
+# LED 핀 번호 설정
 LED1_PIN = 6
 LED2_PIN = 5
+LED3_PIN = 13
+LED4_PIN = 19
 
 def setup_leds():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(LED1_PIN, GPIO.OUT)
     GPIO.setup(LED2_PIN, GPIO.OUT)
+    GPIO.setup(LED3_PIN, GPIO.OUT)
+    GPIO.setup(LED4_PIN, GPIO.OUT)
 
-def control_leds(led1=False, led2=False):
-    GPIO.output(LED1_PIN, GPIO.HIGH if led1 else GPIO.LOW)
-    GPIO.output(LED2_PIN, GPIO.HIGH if led2 else GPIO.LOW)
+def control_leds_by_brightness(brightness):
+    """조도 값에 따라 LED 제어"""
+    if brightness <= 200:
+        # 조도가 200 이하일 때 LED 4개 켜기
+        GPIO.output(LED1_PIN, GPIO.HIGH)
+        GPIO.output(LED2_PIN, GPIO.HIGH)
+        GPIO.output(LED3_PIN, GPIO.HIGH)
+        GPIO.output(LED4_PIN, GPIO.HIGH)
+    else:
+        # 조도가 200 초과일 때 LED 2개만 켜기
+        GPIO.output(LED1_PIN, GPIO.HIGH)
+        GPIO.output(LED2_PIN, GPIO.HIGH)
+        GPIO.output(LED3_PIN, GPIO.LOW)
+        GPIO.output(LED4_PIN, GPIO.LOW)
